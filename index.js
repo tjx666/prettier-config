3a98d30f5ffda30610e6722a50c9d1a1f8b03d34
changfeng <changfeng@gaoding.com>
2023-05-28 03:24:25 +0800

    docs: add some useful comments

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const plugins = [
+    '@prettier/plugin-xml',
+    // some plugins maybe added in the future
+    // https://github.com/hosseinmd/prettier-plugin-jsdoc/issues/135
+    // https://github.com/un-ts/prettier/tree/master/packages/sh
+    // https://github.com/un-ts/prettier/tree/master/packages/pkg
+    // https://github.com/un-ts/prettier/tree/master/packages/autocorrect
+];
+
 module.exports = {
     endOfLine: 'auto',
     printWidth: 100,
@@ -6,8 +15,9 @@ module.exports = {
     singleQuote: true,
     tabWidth: 4,
     trailingComma: 'all',
-    plugins: ['@prettier/plugin-xml'].map((plugin) => require.resolve(plugin)),
+    plugins: plugins.map((plugin) => require.resolve(plugin)),
     overrides: [
+        // https://github.com/prettier/prettier/issues/5322#issuecomment-1276302630
         {
             files: '*.svg',
             options: {
